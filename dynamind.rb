dae0abc5e3965ba83f669f8f460ca5eb8c5d000e
Christian Urich <christian.urich@gmail.com>
2015-08-11 18:21:29 +1000

    Update dynamind.rb

--- dynamind.rb
+++ dynamind.rb
@@ -30,7 +30,7 @@ class Dynamind < Formula
     system "pip install scipy"
     system "pip install sphinx"
     system "pip install sphinx_rtd_theme"
-    system "cmake", ".", *std_cmake_args, "-DWITH_PLUGIN_GDALMODULE=True",  "-DUSEQT5=ON", "-DWITH_DOC=True",  "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'",
+    system "cmake", ".", *std_cmake_args, "-DWITH_PLUGIN_GDALMODULE=True", -DSQLITE3_INCLUDE_DIR="/usr/local/opt/sqlite/include", -DSQLITE3_LIBRARY="/usr/local/opt/sqlite/lib/libsqlite3.dylib", "-DUSEQT5=ON", "-DWITH_DOC=True",  "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'",
       "-DPYTHON_INCLUDE_DIR='#{%x(python-config --prefix).chomp}/include/python2.7'",  "-DWITH_VIEWER=OFF", "-DWITH_PLUGIN_GDALMODULE=ON", "-DWITH_PLUGIN_GDALDRAINAGE=ON",  "-DWITH_PLUGIN_PERFORMANCE_ASSESSMENT=True"
     
     
