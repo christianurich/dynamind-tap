0b6146b0b4c682df585702071764a3ae0a667c36
Christian Urich <christian.urich@gmail.com>
2017-11-14 16:55:44 +1100

    update to latest version

--- dynamind.rb
+++ dynamind.rb
@@ -6,14 +6,14 @@ class Dynamind < Formula
   url "https://github.com/iut-ibk/DynaMind-ToolBox/archive/v0.10.5.zip"
   version "0.10.5"
   sha256 "4ae2d92fffdc1a95e040866cd41509e2e91606298521c02c1deb5a8fbc8c8353"
-  head "https://github.com/iut-ibk/DynaMind-ToolBox.git", :branch => 'V0.10.0'
+  head "https://github.com/christianurich/DynaMind-ToolBox.git", :branch => 'master'
   #head "https://github.com/iut-ibk/DynaMind-ToolBox.git", :branch => 'sqlite_extension'
   
   depends_on "cmake" => :build
   depends_on "git" => :build
-  depends_on "libqglviewer"
+  #depends_on "libqglviewer"
   depends_on "postgis"
-  depends_on "gdal"
+  depends_on "gdal2" => ["with-postgresql"]
   depends_on "libspatialite"
   depends_on "cgal"
   depends_on "qt5"
@@ -22,7 +22,7 @@ class Dynamind < Formula
   depends_on "netcdf"
   depends_on "geos"
   depends_on "swig"
-  depends_on "sqlite"
+  depends_on "sqlite" => ["with-functions"]
   depends_on "python" => :recommended
 
   def install
@@ -36,8 +36,8 @@ class Dynamind < Formula
     system "pip install psycopg2"
     system "pip install dropbox"
     
-    system "cmake", ".", *std_cmake_args, "-DWITH_PLUGIN_GDALMODULE=True", "-DSQLITE3_INCLUDE_DIR=/usr/local/opt/sqlite/include", "-DSQLITE3_LIBRARY=/usr/local/opt/sqlite/lib/libsqlite3.dylib", "-DUSEQT5=ON", "-DWITH_DOC=True",  "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'",
-      "-DPYTHON_INCLUDE_DIR='#{%x(python-config --prefix).chomp}/include/python2.7'",  "-DWITH_VIEWER=OFF", "-DWITH_PLUGIN_GDALMODULE=ON", "-DWITH_PLUGIN_GDALDRAINAGE=ON",  "-DWITH_PLUGIN_PERFORMANCE_ASSESSMENT=True", "-DWITH_PLUGIN_BASICMODULES=OFF"
+    system "cmake", ".", *std_cmake_args, "-DWITH_PLUGIN_GDALMODULE=True", "-DSQLITE3_INCLUDE_DIR=/usr/local/opt/sqlite/include", "-DSQLITE3_LIBRARY=/usr/local/opt/sqlite/lib/libsqlite3.dylib", "-DUSEQT5=ON", "-DWITH_DOC=True",  "-DPYTHON_LIBRARY='#{%x(python2-config --prefix).chomp}/lib/libpython2.7.dylib'",
+      "-DPYTHON_INCLUDE_DIR='#{%x(python2-config --prefix).chomp}/include/python2.7'",  "-DWITH_DOC=ON", "-DWITH_VIEWER=OFF" , "-DWITH_PLUGIN_GDALMODULE=ON", "-DWITH_PLUGIN_GDALDRAINAGE=ON",  "-DWITH_PLUGIN_PERFORMANCE_ASSESSMENT=True", "-DWITH_PLUGIN_BASICMODULES=OFF"
     
     
     system "make", "install" 
